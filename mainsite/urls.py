4ea248a3356bbc8a43e504e724309e5d4ceaff9c
Shraddhey Vivek Dukare <shraddheydukare96@gmail.com>
2025-09-16 00:58:46 +0530

    Update urls.py

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,7 +1,13 @@
+# Path: C:\Users\Admin\GCOEAvidNotes\mainsite\urls.py
 
-from django.urls import path # type: ignore
-from . import views
+from django.urls import path
+from . import views  # <-- THIS IS THE CORRECT IMPORT
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('notes/', views.notes_page, name='notes'),
+    path('videos/', views.videos_page, name='videos'),
+    path('pyqs/', views.pyqs_page, name='pyqs'),
+    path('about/', views.about_page, name='about'),
+    path('search/', views.search_page, name='search'),
 ]
