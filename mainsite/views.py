a9ab15614a4f70c9dd4bdaade54010c5ffade063
Shraddhey Vivek Dukare <shraddheydukare96@gmail.com>
2025-09-16 00:58:10 +0530

    Update views.py

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'home.html')
+    """
+    This view renders the homepage (index.html).
+    """
+    return render(request, 'mainsite/index.html')
+
+def notes_page(request):
+    """
+    This view renders the notes page (notes.html).
+    """
+    return render(request, 'mainsite/notes.html')
+
+def videos_page(request):
+    """
+    This view renders the videos page (videos.html).
+    """
+    return render(request, 'mainsite/videos.html')
+
+def pyqs_page(request):
+    """
+    This view renders the Previous Year Questions page (pyqs.html).
+    """
+    return render(request, 'mainsite/pyqs.html')
+
+def about_page(request):
+    """
+    This view renders the about page (about.html).
+    """
+    return render(request, 'mainsite/about.html')
+
+def search_page(request):
+    """
+    This view renders the search results page (search.html).
+    """
+    return render(request, 'mainsite/search.html')
